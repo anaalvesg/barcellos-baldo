34c8457ffc97a8e031bfeba57cac51f67f6c052a
Ani Alves <129322594+anaalvesg@users.noreply.github.com>
2026-01-28 11:52:46 -0300

    desgraca veia

--- globalscripts.js
+++ globalscripts.js
@@ -5,3 +5,28 @@
   }
 
   updateYear();
+
+// GSAP
+gsap.registerPlugin(ScrollTrigger);
+
+const cards = gsap.utils.toArray(".chooseCards");
+
+gsap.fromTo(
+  cards,
+  {
+    opacity: 0,
+    x: -40
+  },
+  {
+    opacity: 1,
+    x: 0,
+    duration: 0.2,
+    ease: "power2.out",
+    stagger: 0.25,
+    scrollTrigger: {
+      trigger: "#choose",
+      start: "top 80%",
+      toggleActions: "play none none none"
+    }
+  }
+);
