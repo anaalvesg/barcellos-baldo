777a883f4a97c0445af970b915afdd03ae61f010
ani <129322594+anaalvesg@users.noreply.github.com>
2026-01-28 16:00:55 -0300

    wip: ajusta responsividade

--- globalscripts.js
+++ globalscripts.js
@@ -7,26 +7,51 @@
   updateYear();
 
 // GSAP
+// gsap.registerPlugin(ScrollTrigger);
+
+// const cards = gsap.utils.toArray(".chooseCards");
+
+// gsap.fromTo(
+//   cards,
+//   {
+//     opacity: 0,
+//     x: -40
+//   },
+//   {
+//     opacity: 1,
+//     x: 0,
+//     duration: 0.2,
+//     ease: "power2.out",
+//     stagger: 0.25,
+//     scrollTrigger: {
+//       trigger: "#choose",
+//       start: "top 80%",
+//       toggleActions: "play none none none"
+//     }
+//   }
+// );
+
 gsap.registerPlugin(ScrollTrigger);
 
-const cards = gsap.utils.toArray(".chooseCards");
+const mm = gsap.matchMedia();
 
-gsap.fromTo(
-  cards,
-  {
-    opacity: 0,
-    x: -40
-  },
-  {
-    opacity: 1,
-    x: 0,
-    duration: 0.2,
-    ease: "power2.out",
-    stagger: 0.25,
-    scrollTrigger: {
-      trigger: "#choose",
-      start: "top 80%",
-      toggleActions: "play none none none"
+mm.add("(min-width: 992px)", () => {
+  const cards = gsap.utils.toArray(".chooseCards");
+
+  gsap.fromTo(
+    cards,
+    { opacity: 0, x: -40 },
+    {
+      opacity: 1,
+      x: 0,
+      duration: 0.2,
+      ease: "power2.out",
+      stagger: 0.25,
+      scrollTrigger: {
+        trigger: "#choose",
+        start: "top 80%",
+        toggleActions: "play none none none"
+      }
     }
-  }
-);
+  );
+});
